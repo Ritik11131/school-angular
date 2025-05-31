f9c7e76ade445e8991bfa0c0fc3d15a2c1b4bfd4
Ritik11131 <ritik11131@gmail.com>
2025-05-31 14:31:01 +0530

    Route Creator

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  authUrl: 'https://auth.navsat.in/api/v1',
-  apiUrl: 'https://management.navsat.in/api/v1',
+  authUrl: 'https://schoolapi.livetrack24.in/api',
+  apiUrl: 'https://schoolapi.livetrack24.in/api',
   CLIENT_ID: '6e9aa5f7835c15ac4a6d6edd906c9a59',
   CLIENT_SECRET: '844c6535a7f77bc68ba5527b3603e3d1740f904fc64d77e5dfc5c4941a765a0a',
   };
